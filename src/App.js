682d1bdadfc0106de6828aea9f8a55d593c15469
Adam Ashmori <adamashmori@Adams-MacBook-Pro.local>
2023-07-05 16:52:48 +0100

    layout is edited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import react
 import './App.css';
+import { DefaultLayout } from './layout/DefaultLayout';
 import { Entry } from './pages/entry/Entry.page';
 
 
@@ -8,7 +9,12 @@ import { Entry } from './pages/entry/Entry.page';
 function App() {
   return (
     <div className="App">
-      <Entry/>
+      {/* <Entry/> */}
+      <DefaultLayout>
+      //dashboard
+
+      </DefaultLayout>
+      
     </div>
   );
 }
---
+++ src/layout/partials/Footer.comp.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div className="text-center copy-right">
+        &copy; Snotdog all right reserved -2023.
+        </div>
+        
+    )
+}
